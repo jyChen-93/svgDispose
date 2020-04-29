58112674d077cbd0464e33a84af7515ed7c041c4
cjy1137534522 <1137534522@qq.com>
2020-04-29 15:48:50 +0800

    'update'

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     //当运行 build 时生成的生产环境构建文件的目录
     outputDir: 'dist',
     //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
-    assetsDir: './',
+    assetsDir: '/',
     devServer: {
         port: 8333, // 端口
         overlay: {
